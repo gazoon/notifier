5f8c677681479201084f1fa9a93e3f77830dfc4f
gazoon <g89250741413@gmail.com>
2017-09-24 18:20:10 +0300

    fix config

--- config/config.go
+++ config/config.go
@@ -12,16 +12,16 @@ var (
 )
 
 type ServiceConfig struct {
-	config.BaseConfig
-	NotifyYourself             bool                     `json:"notify_yourself"`
-	Neo                        *config.DatabaseSettings `json:"neo"`
-	MongoNotification          *config.MongoQueue    `json:"mongo_notification"`
-	MongoMessages              *config.MongoQueue    `json:"mongo_messages"`
-	MongoNotificationsRegistry *config.MongoDBSettings `json:"mongo_registry"`
-	Telegram                   *config.TelegramSettings `json:"telegram"`
-	TelegramPolling            *config.TelegramPolling  `json:"telegram_polling"`
-	Logging                    *config.Logging          `json:"logging"`
-	GoogleAPI                  *config.GoogleAPI        `json:"google_api"`
+	config.BaseConfig          `mapstructure:",squash" json:",inline"`
+	NotifyYourself             bool                     `mapstructure:"notify_yourself"json:"notify_yourself"`
+	Neo                        *config.DatabaseSettings `mapstructure:"neo" json:"neo"`
+	MongoNotification          *config.MongoQueue       `mapstructure:"mongo_notification" json:"mongo_notification"`
+	MongoMessages              *config.MongoQueue       `mapstructure:"mongo_messages" json:"mongo_messages"`
+	MongoNotificationsRegistry *config.MongoDBSettings  `mapstructure:"mongo_registry" json:"mongo_registry"`
+	Telegram                   *config.TelegramSettings `mapstructure:"telegram" json:"telegram"`
+	TelegramPolling            *config.TelegramPolling  `mapstructure:"telegram_polling" json:"telegram_polling"`
+	Logging                    *config.Logging          `mapstructure:"logging" json:"logging"`
+	GoogleAPI                  *config.GoogleAPI        `mapstructure:"google_api" json:"google_api"`
 }
 
 func Initialization(configPath string) {
